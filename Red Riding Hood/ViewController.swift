f4e84f193bab4f9c12d9f44497a028d0857b19af
Chris Jarvi <mybaldhead@hotmail.com>
2016-10-11 09:21:19 -0700

    Initial Commit

---
+++ Red Riding Hood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red Riding Hood
+//
+//  Created by Chris Jarvi on 10/11/16.
+//  Copyright © 2016 Chris Jarvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
